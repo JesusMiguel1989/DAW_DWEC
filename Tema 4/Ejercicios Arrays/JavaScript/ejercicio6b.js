843d7fe356e2ae1f89c6bd6308da5a6405d9fe81
DESKTOP-UF2VPE7\Jesus <jes11989@hotmil.com>
2022-11-02 08:24:49 +0100

    loteria

--- Tema 4/Ejercicios Arrays/JavaScript/ejercicio6b.js
+++ Tema 4/Ejercicios Arrays/JavaScript/ejercicio6b.js
@@ -1,7 +1,7 @@
 /*
 Crea una aplicación web que muestre una combinación de 7 números distintos del 1 al 9.
 */
-import foto1 from '../estilos/bingo.png';
+
 
 let numeros = [];//donde se guardaran los numeros
 let i, j, aux = 0;//variable del programa
@@ -10,6 +10,7 @@ let cadena = "";//variable cadena para la impresion
 
 let caja = document.getElementById("texto");//variable del textarea
 let boton = document.getElementById("boton");//variable boton
+let btn = document.getElementById("btn");
 let foto = document.getElementById("gif");//variable img
 
 //metemos el 1 numero porque no puede haber otro igual dentro
@@ -37,6 +38,30 @@ boton.addEventListener("click", () => {
         cadena += numeros[i] + " ";
     }
     caja.textContent = cadena;
-    foto.setAttribute('src',foto1);
+    foto.setAttribute('src',"estilos/bingo.gif");
+    btn.style.display = "block";
+    boton.style.display = "none";
+    cadena="";
 })
 
+btn.addEventListener("click",() =>{
+    numeros[0] = Math.floor(Math.random() * (10 - 1) + 1);
+    for (i = 1; i < 7; i++) {
+        //comprobacion
+        do {
+            esta = false;
+            aux = Math.floor(Math.random() * (10 - 1) + 1);
+            for (j = 0; j < numeros.length; j++) {
+                if (aux == numeros[j]) {
+                    esta = true;
+                }
+            }
+        } while (esta == true);
+        
+        //insercion si no esta repetido
+        numeros[i] = aux;
+    }
+    foto.setAttribute('src',"");
+    btn.style.display = "none";
+    boton.style.display = "block";
+})
