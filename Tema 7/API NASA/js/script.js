a9207fbf792737a88ee059c165a8a5dfa43d9e41
DESKTOP-UF2VPE7\Jesus <jes11989@hotmil.com>
2023-02-26 11:37:45 +0100

    API nasa 4.0

--- Tema 7/API NASA/js/script.js
+++ Tema 7/API NASA/js/script.js
@@ -1,6 +1,9 @@
 let fecha = new Date();
 let evento = new Event("click");
 let centinela = false;
+let meteorito = 0;
+let topeMeteoritos = 1000;
+let arrayBolas = [];
 
 async function fotoDiaNasa(fecha) {
     let cuerpo = document.getElementById("cuerpo");
@@ -61,6 +64,73 @@ async function Marte() {
 
 }
 
+async function Meteoros(num) {
+
+    let nuevodiv = document.createElement("div");
+    nuevodiv.id = "datos";
+    nuevodiv.classList.add("datos");
+    cuerpo.appendChild(nuevodiv);
+
+    //creo un div para el mapa
+    let mapa = document.createElement("div");
+    mapa.id = "map";
+    cuerpo.appendChild(mapa);
+
+    let response = await fetch("https://ssd-api.jpl.nasa.gov/fireball.api");
+    let texto = await response.json();
+
+    /* for (let i = 0; i < texto.data.length; i++) {
+        arrayBolas.push(texto.data[i]);
+    } */
+    topeMeteoritos = texto.count;
+
+    let latitud = texto.data[num][3];
+    if (texto.data[num][4] == "S") {
+        latitud *= -1;
+    }
+
+    let longitud = texto.data[num][5];
+    if (texto.data[num][6] == "W") {
+        longitud *= -1;
+    }
+    let fecha = new Date(texto.data[num][0]);
+    console.log(fecha.getMinutes());
+
+    response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud + "&appid=027c50a03ea971eac02eee0dd13efc1d&units=metric&dt=" + fecha.getTime() / 1000);
+    let texto2 = await response.json();
+
+    let datos = document.getElementById("datos");
+    let dato = document.createElement("table");
+    dato.innerHTML = "<tr><td>Momento impacto</td><td>" + texto.data[num][0] + "</td></tr>" +
+        "<tr><td>Energia </td><td>" + texto.data[num][1] + " J</td></tr>" +
+        "<tr><td>Energia del impacto</td><td>" + texto.data[num][2] + " J</td></tr>" +
+        "<tr><td>Latitud</td><td>" + texto.data[num][3] + " Grados</td></tr>" +
+        "<tr><td>Direccion Latitud</td><td>" + texto.data[num][4] + "</td></tr>" +
+        "<tr><td>Longitud</td><td>" + texto.data[num][5] + " Grados</td></tr>" +
+        "<tr><td>Direccion Longitud</td><td>" + texto.data[num][6] + "</td></tr>" +
+        "<tr><td>Dimension</td><td>" + texto.data[num][7] + "</td></tr>" +
+        "<tr><td>Velocidad</td><td>" + texto.data[num][8] + " m/s</td></tr>" +
+        "<tr><td>Tiempo</td><td>" + texto2.weather[0].main + "</td></tr>" +
+        "<tr><td>Velocidad del Viento</td><td>" + texto2.wind.speed + " Km/h</td></tr>" +
+        "<tr><td>Humedad</td><td>" + texto2.main.humidity + " %</td></tr>";
+
+    
+    datos.appendChild(dato);
+
+    //div mapa
+    let map = L.map('map').setView([latitud, longitud], 3);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    //punto
+    let marker = L.marker([latitud, longitud]).addTo(map);
+
+
+}
+
 function borrar() {
     let cuerpo = document.getElementById("cuerpo");
     let body = document.getElementsByTagName("body");
@@ -117,7 +187,7 @@ window.addEventListener("load", () => {
     let fotoDia = document.getElementById("btnFoto");
     let marte = document.getElementById("btnMarte");
 
-    let marteSelector=document.getElementById("selectMarte");
+    let marteSelector = document.getElementById("selectMarte");
     let bolas = document.getElementById("btnBolas");
 
     let cuerpo = document.getElementById("cuerpo");
@@ -187,8 +257,49 @@ window.addEventListener("load", () => {
 
     })
 
-    selectMarte.addEventListener("change",()=>{
+    /* selectMarte.addEventListener("change",()=>{
         
+    }) */
+
+    bolas.addEventListener("click", () => {
+        borrar();
+
+        let cuerpo = document.getElementById("cuerpo");
+        let padre = cuerpo.parentElement;
+        let btndiv = document.createElement("div");
+        btndiv.innerHTML = "<input type='button' class='btnizq' value='Anterior' id='anteriorMeteorito'>";
+        btndiv.innerHTML += "<input type='button' class='btndrc' value='Siguiente' id='siguienteMeteorito'>";
+        padre.appendChild(btndiv);
+
+
+        //creo un div para los datos
+
+
+        //saco los id de los botones
+        let anterior = document.getElementById("anteriorMeteorito");
+        let siguiente = document.getElementById("siguienteMeteorito");
+
+        anterior.addEventListener("click", (e) => {
+            if (meteorito > 0) {
+                meteorito--;
+                borrar();
+                Meteoros(meteorito);
+            } else {
+                e.preventDefault();
+            }
+        });//boton anterior
+
+        siguiente.addEventListener("click", (e) => {
+            if (meteorito == topeMeteoritos) {
+                e.preventDefault();
+            } else {
+                meteorito++;
+                borrar();
+                Meteoros(meteorito);
+            }
+        });//boton siguiente
+
+        Meteoros(meteorito);
     })
 
 
